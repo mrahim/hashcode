3835b2191ccf3e02c4d8a04adac56fb4962cd960
Arthur Mensch <arthur.mensch@m4x.org>
2016-02-11 19:46:09 +0100

    ceil

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from math import sqrt
+from math import sqrt, ceil
+
 
 def distance(x, y):
-    return sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
+    return ceil(sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2))
