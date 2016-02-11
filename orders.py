96c6266bbed51a9ca44c62e65752f21e8c61b963
Arthur Mensch <arthur.mensch@m4x.org>
2016-02-11 20:06:06 +0100

    Object oriented

--- orders.py
+++ orders.py
@@ -1,11 +1,21 @@
+class Game:
+    def __init__(self, orders, warehouses, drones):
+        self.orders = orders
+        self.warhouses = warhouses
+        self.drones = drones
+
+    def get_closest(self):
+
+
+
 class Order:
-    def __init__(self, x, y, products):
+    def __init__(self, x, y, products, game):
         self.x = x
         self.y =y
         self.products = products
 
     def deliver(self, product_id, n_product):
-
+        self.products[product_id] -= n_product
 
     def get_warehouses(self):
 
