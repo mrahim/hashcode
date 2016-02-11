abc098af98a899d93cf39bbc584e02d6bf751fb9
Arthur Mensch <arthur.mensch@m4x.org>
2016-02-11 20:22:04 +0100

    Object oriented

--- orders.py
+++ orders.py
@@ -1,17 +1,20 @@
+from utils import distance
+
+
 class Game:
     def __init__(self, orders, warehouses, drones):
         self.orders = orders
         self.warehouses = warehouses
         self.drones = drones
 
-    def order_elems(self, ref, elem_type='warehouses'):
+    def order_elems(self, ref, elem_type):
         elems = getattr(self, elem_type)
-        sort(elems, key=lambda t: distance(ref.position, t.position))
+        return sorted(elems, key=lambda t: distance(ref.position, t.position))
 
-    def order_
 
 class Order:
-    def __init__(self, position=(x, y), products=products, game):
+    def __init__(self, position,
+                 products, game):
         self.position = position
         self.products = products
         self.game = game
@@ -20,7 +23,24 @@ class Order:
         self.products[product_id] -= n_product
 
     def get_warehouses(self):
+        warehouses = []
+        for warehouse in self.game.order_elems(self.position, 'warehouses'):
+
+
+    def cost(self):
+
 
 class Warehouse:
+    def __init__(self, position, products, game):
+        self.position = position
+        self.products = products
 
 class Drone:
+    def __init__(self, position, products, game):
+        self.position = position
+        self.products = products
+
+    def deliver(self):
+        self.products[product_id] -= n_product
+
+    def load(self):
