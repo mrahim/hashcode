6724d61fc784a15c1087178265568154326cb239
Arthur Mensch <arthur.mensch@m4x.org>
2016-02-11 20:11:20 +0100

    Object oriented

--- orders.py
+++ orders.py
@@ -1,18 +1,20 @@
 class Game:
     def __init__(self, orders, warehouses, drones):
         self.orders = orders
-        self.warhouses = warhouses
+        self.warehouses = warehouses
         self.drones = drones
 
-    def get_closest(self):
+    def order_elems(self, ref, elem_type='warehouses'):
+        elems = getattr(self, elem_type)
+        sort(elems, key=lambda t: distance(ref.position, t.position))
 
-
+    def order_
 
 class Order:
-    def __init__(self, x, y, products, game):
-        self.x = x
-        self.y =y
+    def __init__(self, position=(x, y), products=products, game):
+        self.position = position
         self.products = products
+        self.game = game
 
     def deliver(self, product_id, n_product):
         self.products[product_id] -= n_product
