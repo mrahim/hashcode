4527721080262459ca67c2466f40ed6a6d8c3a6b
Mehdi Rahim <rahim.mehdi@gmail.com>
2016-02-11 19:47:25 +0100

    weight

--- utils.py
+++ utils.py
@@ -2,3 +2,10 @@ from math import sqrt
 
 def distance(x, y):
     return sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
+    
+    
+def order_weight(products_in_order, product_weights):
+    cost = 0    
+    for product, weight in zip(order, product_weights):
+        cost += product*weight
+    return cost
