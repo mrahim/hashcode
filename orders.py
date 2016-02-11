1dca0b77244f4e411ef508f970b2bde03955ace8
Arthur Mensch <arthur.mensch@m4x.org>
2016-02-11 20:39:41 +0100

    Object oriented

--- orders.py
+++ orders.py
@@ -1,15 +1,31 @@
 from utils import distance
-
+import numpy as np
 
 class Game:
     def __init__(self, orders, warehouses, drones):
         self.orders = orders
         self.warehouses = warehouses
         self.drones = drones
+        self.cost_matrix = np.array(len(orders), len(drones))
+
+
+    def fill_matrix(self):
+        for i, order in enumerate(self.orders):
+            warehouses, cost = order.evaluate()
+            self.cost_matrix[i] = cost
+            for j, drone in enumerate(self.drones):
+                self.cost_matrix[i, j] += drone.evaluate(warehouses)
 
     def order_elems(self, ref, elem_type):
         elems = getattr(self, elem_type)
-        return sorted(elems, key=lambda t: distance(ref.position, t.position))
+        distances = map(elems, lambda t: distance(ref.position, t.position))
+        return sorted(elems, key=distances), sorted(distances)
+
+
+    def turn(self):
+        for drones in self.drones:
+            if drones.is_:
+
 
 
 class Order:
@@ -21,13 +37,21 @@ class Order:
 
     def deliver(self, product_id, n_product):
         self.products[product_id] -= n_product
+        if np.all(self.products <= 0):
+            return True
+        else:
+            return False
 
-    def get_warehouses(self):
-        warehouses = []
-        for warehouse in self.game.order_elems(self.position, 'warehouses'):
-
-
-    def cost(self):
+    def evaluate(self):
+        self.warehouses_ = []
+        products = self.products.copy()
+        for warehouse, distance in self.game.order_elems(self.position, 'warehouses'):
+            products -= warehouse.products
+            self.warehouses_.append(warehouse)
+            self.score_ += distance
+            if np.all(self.products) < 0:
+                break
+        return self.warehouses_, self.score_
 
 
 class Warehouse:
@@ -40,7 +64,8 @@ class Drone:
         self.position = position
         self.products = products
 
-    def deliver(self):
+    def deliver(self, product_id, n_product):
         self.products[product_id] -= n_product
 
-    def load(self):
+    def evaluate(self, warehouses):
+        return sum(map(warehouses, lambda t: distance(t.position, self.position)))
