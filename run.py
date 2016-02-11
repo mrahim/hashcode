7f9c9955ec6cd08853e7b9a6725ac483cf4e03e5
Alexandre Abraham <abraham.alexandre@gmail.com>
2016-02-11 19:26:24 +0100

    Fix some loading

--- run.py
+++ run.py
@@ -13,8 +13,6 @@ def read_n_int(f, n):
     if len(values) != n:
         raise ValueError('Expected number of values is not right in line %i' %
                          global_line)
-    if n == 1:
-        values = values[0]
     return values
 
 if len(sys.argv) < 2:
@@ -22,9 +20,9 @@ if len(sys.argv) < 2:
 
 with open(sys.argv[1]) as f:
     n_rows, n_cols, n_drones, n_turns, max_payload = read_n_int(f, 5)
-    n_product_types = read_n_int(f, 1)
+    n_product_types = read_n_int(f, 1)[0]
     product_weights = read_n_int(f, n_product_types)
-    n_warehouses = read_n_int(f, 1)
+    n_warehouses = read_n_int(f, 1)[0]
 
     # List of (x, y, products)
     warehouses = []
@@ -32,14 +30,14 @@ with open(sys.argv[1]) as f:
         x, y = read_n_int(f, 2)
         products = read_n_int(f, n_product_types)
         warehouses.append((x, y, products))
-    n_orders = read_n_int(f, 1)
+    n_orders = read_n_int(f, 1)[0]
 
     # List of (x, y, products)
     orders = []
     for i in range(n_orders):
         x, y = read_n_int(f, 2)
-        n_items = read_n_int(f, 1)
+        n_items = read_n_int(f, 1)[0]
         products = read_n_int(f, n_items)
-        orders.append((x, y, products))
+        orders.append((x, y, np.bincount(products)))
 
-print(locals())
+
