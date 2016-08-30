f4ad662eac6a5f889bdef93eeb50d4b16f1f1ca8
Arthur Mensch <arthur.mensch@m4x.org>
2016-08-30 08:54:12 +0200

    last commit

--- orders.py
+++ orders.py
@@ -1,30 +1,25 @@
 from math import ceil
 
 from utils import distance
-import numpy as np
-
-global MAX_WEIGHT
-global INSTRUCTION
-global WEIGHTS
 
 class Game:
     def __init__(self, orders, warehouses, drones):
         self.orders = orders
         self.warehouses = warehouses
         self.drones = drones
-        self.cost_matrix = np.array(len(orders), len(drones))
+        self.cost_matrix = np.zeros((len(orders), len(drones)))
 
     def fill_matrix(self):
         for i, order in enumerate(self.orders):
-            warehouses, cost = order.evaluate()
+            warehouses, cost = order.evaluate(game)
             self.cost_matrix[i] = cost
             for j, drone in enumerate(self.drones):
                 self.cost_matrix[i, j] += drone.evaluate(warehouses)
 
-    def order_elems(self, ref, elem_type):
-        elems = getattr(self, elem_type)
-        distances = map(elems, lambda t: distance(ref.position, t.position))
-        return sorted(elems, key=distances), sorted(distances)
+    def order_warehouses(self, ref):
+        distances = list(map(lambda t: distance(ref, t.position), self.warehouses))
+        arg_distances = sorted(range(len(distances)), key=lambda t: distances[t])
+        return [self.warehouses[i] for i in arg_distances], [distances[i] for i in arg_distances]
 
     def turn(self):
         for j, drone in enumerate(self.drones):
@@ -32,7 +27,6 @@ class Game:
                 drone.affect(self.orders[np.argmin(self.cost_matrix[:, j])])
             else:
                 drone.wait -= 1
-        self.fill_matrix()
 
 class Order:
     def __init__(self, order_id, position,
@@ -40,6 +34,7 @@ class Order:
         self.id = order_id
         self.position = position
         self.products = products
+        self.score_ = 0
 
     def deliver(self, product_id, n_products):
         self.products[product_id] -= n_products
@@ -48,10 +43,10 @@ class Order:
             n_products += self.products[product_id]
         return n_products
 
-    def evaluate(self):
+    def evaluate(self, game):
         self.warehouses_ = []
         products = self.products.copy()
-        for warehouse, distance in self.game.order_elems(self.position, 'warehouses'):
+        for warehouse, distance in zip(*game.order_warehouses(self.position)):
             products -= warehouse.products
             self.warehouses_.append(warehouse)
             self.score_ += distance
@@ -75,10 +70,10 @@ class Warehouse:
 
 
 class Drone:
-    def __init__(self, drone_id, position, products):
+    def __init__(self, drone_id, position):
         self.id = drone_id
         self.position = position
-        self.products = products
+        self.products = np.zeros(n_product_types)
         self.wait = 0
         self.weight = 0
 
@@ -88,13 +83,13 @@ class Drone:
         else:
             n_products = order.deliver(product_id, self.products[product_id])
             self.products[product_id] -= n_products
-            self.weight -= n_products * self.game.weights[product_id]
+            self.weight -= n_products * product_weights[product_id]
             self.wait += 1
-            INSTRUCTIONS.append([self.id, 'D', order.id, product_id, n_products])
+            instructions.append([self.id, 'D', order.id, product_id, int(n_products)])
             return 1
 
     def evaluate(self, warehouses):
-        return sum(map(warehouses, lambda t: distance(t.position, self.position)))
+        return sum(map(lambda t: distance(t.position, self.position), warehouses))
 
     def flies(self, position):
         self.wait += int(ceil(distance(position, self.position)))
@@ -106,22 +101,24 @@ class Drone:
         self.wait += 1
         n_products = warehouse.loads(product_id, n_product)
 
-        self.weight += n_products * WEIGHTS[product_id]
+        self.weight += n_products * product_weights[product_id]
 
-        if self.weight > MAX_WEIGHTS:
+        if self.weight > max_payload:
             old_n_products = n_products
-            n_products -= (MAX_WEIGHTS - self.weight) // WEIGHTS[product_id] + 1
-            self.weight += (old_n_products - n_products) * WEIGHTS[product_id]
+            n_products -= (max_payload - self.weight) // product_weights[product_id] + 1
+            self.weight += (old_n_products - n_products) * product_weights[product_id]
             self.products += n_products
-            INSTRUCTIONS.append([self.id, 'L', warehouse.id,
-                                 product_id, n_products)
+            instructions.append([self.id, 'L', warehouse.id,
+                                 product_id, int(n_products)])
             return 0
         else:
+            instructions.append([self.id, 'L', warehouse.id,
+                                 product_id, int(n_products)])
             return 1
 
     def affect(self, order):
         for warehouse in order.warehouses_:
-            self.flies(warehouse)
+            self.flies(warehouse.position)
             for product_id in np.where(order.products)[0]:
                 has_space = self.loads(warehouse, product_id,
                                   order.products[product_id])
@@ -131,13 +128,65 @@ class Drone:
             for product_id in np.where(order.products)[0]:
                 self.delivers(order, product_id)
 
+import numpy as np
+import sys
 
-def main():
-    global INSTRUCTION = []
-    global MAX_WEIGHTS = 100
-    global WEIGHTS =
+
+global_line = 0
+
+
+def read_n_int(f, n):
+    global global_line
+    line = f.readline().split()
+    values = [int(x) for x in line]
+    global_line += 1
+    if len(values) != n:
+        raise ValueError('Expected number of values is not right in line %i' %
+                         global_line)
+    return values
+
+if len(sys.argv) < 2:
+    raise ValueError('Usage run.py <file>')
+
+with open(sys.argv[1]) as f:
+    n_rows, n_cols, n_drones, n_turns, max_payload = read_n_int(f, 5)
+    n_product_types = read_n_int(f, 1)[0]
+    product_weights = read_n_int(f, n_product_types)
+    n_warehouses = read_n_int(f, 1)[0]
+
+    # List of (x, y, products)
+    warehouses = []
+    for i in range(n_warehouses):
+        x, y = read_n_int(f, 2)
+        products = read_n_int(f, n_product_types)
+        warehouses.append(((x, y), products))
+    n_orders = read_n_int(f, 1)[0]
+
+    # List of (x, y, products)
     orders = []
     for i in range(n_orders):
-        orders.append(Order(i))
-    for i in range(n_warehouses):
-    for i i
+        x, y = read_n_int(f, 2)
+        n_items = read_n_int(f, 1)[0]
+        products = read_n_int(f, n_items)
+        orders.append(((x, y), np.bincount(products, minlength=n_product_types)))
+
+instructions = []
+orders_list = []
+drones_list = []
+warehouses_list = []
+for i, order in enumerate(orders):
+    orders_list.append(Order(i, order[0], order[1]))
+for i, warehouse in enumerate(warehouses):
+    warehouses_list.append(Warehouse(i, warehouse[0], warehouse[1]))
+for i in range(n_drones):
+    drones_list.append(Drone(i, (0, 0)))
+
+game = Game(orders_list, warehouses_list, drones_list)
+
+game.fill_matrix()
+
+for i in range(n_turns):
+    print(i)
+    game.turn()
+for instruction in instructions:
+    print(instruction)
